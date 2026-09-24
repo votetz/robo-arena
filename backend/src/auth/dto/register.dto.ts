fdfd38a28f9775d310cf9107ccd89be5b3ef4911
bohdan <bogomdannyibogdan@gmail.com>
2026-09-24 15:09:18 +0300

    feat: init backend with auth and postgres setup

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import {IsEmail, IsString, MinLength} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @MinLength(6)
+  password: string;
+}
